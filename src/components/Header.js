9678f4555719743e5af49884e90f5e46350bbe5f
Tanner Kothlow <tannerkothlow@msn.com>
2023-01-30 11:20:43 -0600

    established proper css flex

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,11 @@ import Footer from './Footer';
 function Header(props) {
     return (
         <div className='app-header'>
+            <div className='header-block'>
             <h1>Portfolio</h1>
+            </div>
             <Navigation />
+            <Footer />
         </div>
     )
 }
