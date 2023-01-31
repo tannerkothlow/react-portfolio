1350d0457cf6cb1be6059393ab44c6ff1576dff6
Tanner Kothlow <tannerkothlow@msn.com>
2023-01-31 15:18:54 -0600

    changed images to be background images for proper styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,16 +9,24 @@ import React, { useState } from 'react';
 function Project (props) {
     console.log(props.projects);
     
+    function assignBackgroundImage(img) {
+        const style = {
+            backgroundImage: 'url(' + img + ')',
+            backgroundRepeat: 'no-repeat'
+        };
+        return style;
+    }
 
     return (
         <div className='app-project'>
             {props.projects.map(project => (
-                <div className='app-project-card' key={project.id}>
+                <div className='app-card-holder'>
+                <div style={assignBackgroundImage(project.image)} className='app-project-card' key={project.id}>
                     <div className='card-placard'>
                     <a href={project.url}>{project.title}</a>
                     <p>{project.tech}</p>
                     </div>
-                    <img src={project.image}></img>
+                </div>
                 </div>
             ))}
         </div>
