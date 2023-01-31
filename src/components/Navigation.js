f75bccd8acdf2f4bd359fd8e08c524a0c31e20ef
Tanner Kothlow <tannerkothlow@msn.com>
2023-01-31 11:51:09 -0600

    filler about me text and established contact form logic

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,15 @@ const displayAboutMe = (
     <div className='app-about-me'>
         <h2>About Me</h2>
         <p>Icon</p>
-        <p>About me content.</p>
+        <p>
+            Filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text 
+            filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text 
+        </p>
+        <p>
+            filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text 
+            filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text 
+            filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text 
+        </p>
     </div>
 )
 
@@ -16,11 +24,36 @@ const displayPortfolio = (
     </div>
 )
 
-const displayContact = (
-    <div className='app-contact'>
-        <h2>Contact Me</h2>
-    </div>
-)
+// const displayContact = (
+//     <div className='app-contact'>
+//         <h2>Contact Me</h2>
+//         {/* name, email, message */}
+//         <form className="form">
+//             <input
+//             value={contactName}
+//             name="contactName"
+//             type="text"
+//             placeholder="Name"
+//             />
+//             <input
+//             value={contactEmail}
+//             name="contactEmail"
+//             type="text"
+//             placeholder="Email"
+//             />
+//             <input
+//             value={contactMessage}
+//             name="conactMessage"
+//             onChange={handleInputChange}
+//             type="text"
+//             placeholder="First Name"
+//             />
+//         <button type="button" onClick={handleFormSubmit}>
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+// )
 
 const displayResume = (
     <div className='app-resume'>
@@ -35,6 +68,7 @@ function Navigation(props) {
 
     let display;
 
+    // Checks for any static asset state
     switch(props.display) {
         case 'about-me':
             display = displayAboutMe;
@@ -42,13 +76,72 @@ function Navigation(props) {
         case 'portfolio':
             display = displayPortfolio;
             break;
-        case 'contact':
-            display = displayContact;
-            break;
+        // case 'contact':
+        //     display = displayContact;
+        //     break;
         case 'resume':
             display = displayResume;
             break;
     };
+    // If contact form state, attach JS logic to displayContact before sending it higher up.
+    if (props.display = 'contact') {
+        const [contactName, setContactName] = useState('');
+        const [contactEmail, setContactEmail] = useState('');
+        const [contactMessage, setContactMessage] = useState('');
+
+        const handleInputChange = (e) => {
+            const { name, value } = e.target;
+        
+            return name === 
+            'contactName' ? setContactName(value) 
+            : 'contactEmail' ? setContactEmail(value)
+            : setContactMessage(value);
+          };
+
+        const handleFormSubmit = (e) => {
+            e.preventDefault();
+        
+            console.log(contactName, contactEmail, contactMessage);
+            setContactName('');
+            setContactEmail('');
+            setContactMessage('');
+        };
+
+        const displayContact = (
+            <div className='app-contact'>
+                <h2>Contact Me</h2>
+                {/* name, email, message */}
+                <form className="form">
+                    <input
+                    value={contactName}
+                    name="contactName"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Name"
+                    />
+                    <input
+                    value={contactEmail}
+                    name="contactEmail"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Email"
+                    />
+                    <input
+                    value={contactMessage}
+                    name="conactMessage"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="First Name"
+                    />
+                <button type="button" onClick={handleFormSubmit}>
+                  Submit
+                </button>
+              </form>
+            </div>
+        )
+
+        display = displayContact;
+    }
 
     return (
         <div className='app-navigator'>
